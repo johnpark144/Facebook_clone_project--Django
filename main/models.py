d0bb65329c541841186dfd9842390b30a1c6fb6e
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-09-22 08:49:56 -0500

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,5 +1,3 @@
-from os import link
-from turtle import title
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager
