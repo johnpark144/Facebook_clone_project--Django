bf900080fd7b824bd3aecc847efcdaed908a8c08
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2022-11-21 12:44:29 -0600

    Update settings.py

--- FB_clone/settings.py
+++ FB_clone/settings.py
@@ -108,6 +108,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
